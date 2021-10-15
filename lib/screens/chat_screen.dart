6fc7f15ff78952920fcde92e6d814ba3a0a9887c
L4WLI3T <48093400+L4WLI3T@users.noreply.github.com>
2021-10-16 01:45:18 +0530

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,33 +20,8 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
-  //final _auth = FirebaseAuth.instance;
 
   String messageText;
-  //@override
-  //void initState(){
-    //super.initState();
-    //getCurrentUser();
-  //}
-  //void getCurrentUser() async {
-    //try{
-    //final user = await _auth.currentUser();
-    //if(user != null){
-      //loggedInUser = user;
-    //}}
-    //catch(e){
-      //print(e);
-    //}
-
-  //}
-  // void getMessages() async{
-  //   final messages = await _firestore.collection('messages').getDocuments();
-  //   for(var message in messages.documents){
-  //     print(message.data);
-  //   }
-  // }
-
-  //}
   void messagesStream() async {
     await for(var snapshot in _firestore.collection('messages').snapshots()){
       for(var message in snapshot.documents){
